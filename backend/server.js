ef9c01a1be756560df7820a9e4d26e8f7419eaa3
Othman Faiz <most3mr@live.com>
2022-02-18 17:59:40 +0300

    dayy 11

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,28 @@
-console.log("Server...")
+const express = require("express")
+const colors = require("colors")
+const donenv = require("dotenv").config()
+const {errorHandler} = require("./middleware/errorMiddleware")
+const connectDB = require("./config/db")
+const PORT = process.env.PORT || 5000
+
+// Connect to database
+connectDB()
+
+const app = express()
+
+
+// accepting json data
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get("/", (req, res) => {
+ res.status(200).json({message: "Welcome to the Support desk API"})
+})
+
+//Routes
+app.use("/api/users", require("./routes/userRoutes"))
+
+app.use(errorHandler)
+
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
