975220cf3e2b0d375d06f66d5e60aee0e72c6451
Othman Alomair <most3mr@live.com>
2022-02-20 18:58:21 +0300

    day 13

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -4,6 +4,7 @@ import { FaUser } from "react-icons/fa"
 import { toast } from "react-toastify"
 import { useSelector, useDispatch } from "react-redux"
 import { register, reset } from "../features/auth/authSlice"
+import Spinner from "../components/Spinner"
 
 function Register() {
  const [formData, setFromData] = useState({
@@ -56,6 +57,10 @@ function Register() {
   }
  }
 
+ if(isLoading) {
+   return <Spinner />
+ }
+ 
   return (
     <>
      <section className="headign">
